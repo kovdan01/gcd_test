6ea41d917efaf3dd5a7b9af604cf13af558ac20a
kovdan01 <kovdan01@yandex.ru>
2018-08-04 08:36:06 +0300

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,35 +1,20 @@
-#include <iostream>
 #include <chrono>
-#include <utility>
-#include <functional>
-#include <fstream>
-#include <string>
 #include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <iostream>
 #include <limits>
 #include <random>
-
-//Returns 0 in case of  success
-int gen_data(std::string filename, std::uint64_t pairs_count, std::uint64_t max_element = std::numeric_limits<std::uint64_t>::max())
-{
-    std::ofstream file(filename);
-    if (!file.is_open())
-        return 1;
-    std::mt19937 prng (std::random_device{}());
-    std::uniform_int_distribution<std::uint64_t> dist(1, max_element);
-    int i;
-    for (i = 0; i < pairs_count; ++i)
-        file << dist(prng) << ' ' << dist(prng) << '\n';
-    file.close();
-    return 0;
-}
+#include <string>
+#include <utility>
 
 std::uint64_t gcd(std::uint64_t a, std::uint64_t b)
 {
     if (a > b)
         std::swap(a, b);
-    while (!a)
+    while (a)
     {
-        b %= a;
+        b %= a; //NOLINT clang-analyzer-core.DivideZero
         std::swap(a, b);
     }
     return b;
@@ -96,8 +81,7 @@ std::uint64_t gcd4(std::uint64_t a, std::uint64_t b)
 	if ((~a & 1) != 0) {
 		if ((b & 1) != 0)
 			return gcd4(a >> 1, b);
-		else
-			return gcd4(a >> 1, b >> 1) << 1;
+		return gcd4(a >> 1, b >> 1) << 1;
 	}
 	if ((~b & 1) != 0)
 		return gcd4(a, b >> 1);
@@ -109,20 +93,20 @@ std::uint64_t gcd4(std::uint64_t a, std::uint64_t b)
 std::uint64_t gcd5(std::uint64_t a, std::uint64_t b) 
 {
     std::uint64_t shift;
-	if (a == 0)
+	if (!a)
 		return b;
-	if (b == 0)
+	if (!b)
 		return a;
-	for (shift = 0; ((a | b) & 1) == 0; ++shift) 
+	for (shift = 0; !((a | b) & 1); ++shift) 
     {
 		a >>= 1;
 		b >>= 1;
 	}
-	while ((a & 1) == 0)
+	while (!(a & 1))
 		a >>= 1;
     do 
     {
-        while ((b & 1) == 0)
+        while (!(b & 1))
             b >>= 1;
         if (a > b) 
         {
@@ -131,25 +115,25 @@ std::uint64_t gcd5(std::uint64_t a, std::uint64_t b)
             a = t;
         }
         b = b - a;
-    } while (b != 0);
+    } while (b);
 	return a << shift;
 }
 
 std::uint64_t gcd6 (std::uint64_t a, std::uint64_t b) 
 {
     std::uint64_t shift;
-    if (a == 0 || b == 0)
+    if (!a || !b)
         return a | b;
-    for (shift = 0; ((a | b) & 1) == 0; ++shift) 
+    for (shift = 0; !((a | b) & 1); ++shift) 
     {
         a >>= 1;
         b >>= 1;
     }
-    while ((a & 1) == 0)
+    while (!(a & 1))
         a >>= 1;
     do 
     {
-        while ((b & 1) == 0) 
+        while (!(b & 1)) 
             b >>= 1;
         if (a < b) 
             b -= a;
@@ -160,18 +144,27 @@ std::uint64_t gcd6 (std::uint64_t a, std::uint64_t b)
             b = diff;
         }
         b >>= 1;
-    } while (b != 0);
+    } while (b);
     return a << shift;
 }
 
-std::uint64_t gcd7(std::uint64_t a, std::uint64_t b)
+//Returns 0 in case of  success
+int gen_data(const std::string& filename, std::uint64_t pairs_count, std::uint64_t max_element = std::numeric_limits<std::uint64_t>::max())
 {
-    while (b^=a^=b^=a%=b);
-    return a;
+    std::ofstream file(filename);
+    if (!file.is_open())
+        return 1;
+    std::mt19937 prng (std::random_device{}());
+    std::uniform_int_distribution<std::uint64_t> dist(1, max_element);
+    int i;
+    for (i = 0; i < pairs_count; ++i)
+        file << dist(prng) << ' ' << dist(prng) << '\n';
+    file.close();
+    return 0;
 }
 
 //Returns number of milliseconds in case of success, -1 otherwise
-std::int64_t test_algo(std::string input_filename, std::function<std::uint64_t (std::uint64_t, std::uint64_t)> func)
+std::int64_t test_algo(const std::string& input_filename, const std::function<std::uint64_t (std::uint64_t, std::uint64_t)>& func)
 {
     std::ifstream file (input_filename);
     if (!file.is_open())
@@ -202,7 +195,7 @@ void help()
                  "If the 3rd argument is not specified, the default value 2^64-1 will be used\n\n";
 }
 
-void file_error(std::string filename)
+void file_error(const std::string& filename)
 {
     std::cout << "An error occured while opening " << filename << " file\n";
 }
@@ -216,14 +209,15 @@ int main(int argc, char* argv[])
         return 1;
     }
     filename = std::string(argv[1]);
-    if (!filename.compare(std::string("--help")))
+    if (filename == std::string("--help"))
     {
         help();
         return 0;
     }
     if (argc == 3)
     {
-        if (gen_data(filename, std::atoll(argv[2])))
+        char *end;
+        if (gen_data(filename, std::strtoull(argv[2], &end, 10)))
         {
             file_error(filename);
             return 1;
@@ -231,7 +225,8 @@ int main(int argc, char* argv[])
     }
     else if (argc > 3)
     {
-        if (gen_data(filename, std::atoll(argv[2]), std::atoll(argv[3])))
+        char *end;
+        if (gen_data(filename, std::strtoull(argv[2], &end, 10), std::strtoull(argv[3], &end, 10)))
         {
             file_error(filename);
             return 1;
@@ -239,8 +234,8 @@ int main(int argc, char* argv[])
     }
     std::int64_t gcd_time = test_algo(filename, gcd), bin_gcd_time = test_algo(filename, bin_gcd), 
             gcd3_time = test_algo(filename, gcd3), gcd4_time = test_algo(filename, gcd4),
-            gcd5_time = test_algo(filename, gcd5), gcd6_time = test_algo(filename, gcd6), gcd7_time = test_algo(filename, gcd7);
-    if (gcd_time == -1 || bin_gcd_time == -1 || gcd3_time == -1 || gcd4_time == -1 || gcd5_time == -1 || gcd6_time == -1 || gcd7_time == -1)
+            gcd5_time = test_algo(filename, gcd5), gcd6_time = test_algo(filename, gcd6);
+    if (gcd_time == -1 || bin_gcd_time == -1 || gcd3_time == -1 || gcd4_time == -1 || gcd5_time == -1 || gcd6_time == -1)
     {
         file_error(filename);
         return 1;
@@ -251,6 +246,5 @@ int main(int argc, char* argv[])
                  "\tgcd3\t\t"<< gcd3_time << "\n"
                  "\tgcd4\t\t"<< gcd4_time << "\n"
                  "\tgcd5\t\t"<< gcd5_time << "\n"
-                 "\tgcd6\t\t"<< gcd6_time << "\n"
-                 "\tgcd7\t\t"<< gcd7_time << '\n';
+                 "\tgcd6\t\t"<< gcd6_time << '\n';
 }
